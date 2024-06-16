8af39e08025f0789f5e5a68a37cf35bac38dd6ee
Sainath More <166020011+saiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-06-16 20:14:39 +0000

    datatypes in javascript

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+let name = "Sainath" // String Type
+let age = 21 // Number Type
+let isLoggedIn = true // Boolean Type
+
+
+// Number 
+// bigint
+// string
+// boolean 
+// null
+//undefined
+// symbol 
+// Object
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(isLoggedIn);
+console.log(typeof(null));  // Type of null will always be an object 
+console.log(typeof(undefined)); // Type of undefined will be an undefined
