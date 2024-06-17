bcd00f32acf6b9f49faef11b5dfbc3b35bdab62d
Sainath More <166020011+saiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-06-17 07:45:03 +0000

    updates in conversion of datatypes and operations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -20,4 +20,36 @@ let valueLoggedIn = Boolean(LoggedIn);
 
 let someNumber = 33;
 let Somestring = String(someNumber);
-console.log(typeof Somestring) 
+// console.log(typeof Somestring) 
+
+
+
+// *********************************************************** Operations **********************************************************************************
+
+let value = 3;
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "Hello";
+let str2 = " Sainath";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + "2");
+
+
+
+let  num1 = 5;
+// let addnum1 = num1++; // If you assigned an increment operator postfixly then it will perform an  addition operation  
+let addnum1 = ++num1;  // And if you assigened an increment operator prefixley then still it wll give same output it will perform an addition operation 
+// console.log(addnum1); 
+console.log(num1);
