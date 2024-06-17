f9daf9c96d2eeb65c89ed1183de4947ac11ba570
Sainath More <166020011+saiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-06-17 18:38:50 +0000

    String and it's Methods

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,22 @@
+const name = "Sainath";
+const age = 21;
+// console.log("my Name is " + name + "and I am " + age + "year's old") // This is wrong approach 
+// console.log(`my name is ${name} and i am ${age} year's old`) // This is String Interpolation a correct approach 
+
+const myFullname = new String("Sai-nath-More") // A different way to declare the siring 
+// console.log(myFullname[0]);
+// console.log(myFullname.__proto__);
+// console.log(myFullname.length);
+// console.log(myFullname.toUpperCase());
+// console.log(myFullname.toLowerCase());
+// console.log(myFullname.charAt(1));
+// console.log(myFullname.indexOf('i'));
+// console.log(myFullname.substring(0,3));
+// console.log(myFullname.slice(1,3));
+// console.log(myFullname.trim());
+console.log(myFullname.split('-'))
+
+
+const url = "https://laughing-doodle-69v7wq5v9vpvfxj7.github.dev/" //This is a string (example)
+// console.log(url.replace('.' , '-'))
+// console.log(url.includes('laughing'));
