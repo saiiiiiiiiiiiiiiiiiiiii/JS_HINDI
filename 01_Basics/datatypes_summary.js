040165e2aef1d3738f224fefb7415aa2c965a60c
Sainath More <166020011+saiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-06-17 11:07:43 +0000

    Datatypes Summary

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,32 @@
+// primitive Datatype
+// There are 7 Primitive Datatypes 1) string, 2) Number, 3) Boolearn, 4)null, 5)undefined, 6)symbol, 7)BigInt
+
+let string1 = "Sainath"; // This is String
+let num = 21;   // This is Number
+let isLoggedIn = true; // This is Boolean
+let value = null; // This is null
+let email; // This is Undefined
+const symb = Symbol('123'); // This is syymbol
+const symb1 = Symbol('123');// This is Symbol
+console.log(symb === symb1);
+
+const BigNumber = 6494149419649619849619849619849196
+
+// Reference Type (Non Primitive Datatypes)
+// 1) Array, 2)Objects 3)Functions  
+// 1)Array
+const Heros = ["Shaktiman","Krish","Hanuman"] 
+
+// 2)Object
+const obj = {
+    name : "Sainath",
+    age : "21"
+}
+
+// 3)Function
+const myfunction = function () {
+    console.log("Hello World");
+}
+console.log(typeof(value))
+
+
