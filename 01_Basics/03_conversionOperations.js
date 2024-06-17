58eeeed13d0cba0af72f13e936132fd6f4e4618c
Sainath More <166020011+saiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-06-17 07:11:52 +0000

    Conversion of Datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score = null;
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+
+// "33" is easily get converted into number 33
+// "33abc" is not get converted into number so it it will give a value NaN 
+
+let LoggedIn = "Sainath";
+let valueLoggedIn = Boolean(LoggedIn);
+// console.log(valueLoggedIn);
+// The value of 1 in Boolean is True
+// The value of 0 in Boolean is False
+// The value of empty string is False
+// The value of String is True
+
+
+let someNumber = 33;
+let Somestring = String(someNumber);
+console.log(typeof Somestring) 
