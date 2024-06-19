9a34e46d14283770cb95bd0977c40a190bff9477
Sainath More <166020011+saiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-06-19 22:19:40 +0530

    Update 01_array.js

--- 02_Basics/01_array.js
+++ 02_Basics/01_array.js
@@ -34,3 +34,37 @@ console.log(mynewarray2);
 console.log("c ",myArr );
 
 
+const Marvel_Heros = ["Ironman","Thor","SpiderMan"]
+const DC_Heros = ["SuperMan","WonderWomen","BatMan"]
+// Marvel_Heros.push(DC_Heros)
+// console.log(Marvel_Heros);
+// console.log(Marvel_Heros[3][2]);
+
+// let real_heros = Marvel_Heros.concat(DC_Heros);
+// console.log(real_heros);
+
+// Spread Operator in array 
+// const all_New_heros = [...Marvel_Heros,...DC_Heros];
+// console.log(all_New_heros);
+
+
+// const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array =  another_array.flat(Infinity)
+// console.log(real_another_array);
+console.log(Array.isArray("Sainath"));// Through this Method you can check waether the given data is an array or not
+console.log(Array.from("Sainath"))//Through this Method you can convert the given Data into array 
+
+const score1 = 100;
+const score2 = 200;
+const score3 = 200;
+
+console.log(Array.of(score1,score2,score3));
+
+
+
+
+
+
+
+
+
