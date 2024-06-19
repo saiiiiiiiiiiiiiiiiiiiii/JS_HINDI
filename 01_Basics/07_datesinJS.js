390dc3140a839c7ec69550a14a964aca6a01028f
Sainath More <166020011+saiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-06-19 20:09:39 +0530

    Update 07_datesinJS.js

--- 01_Basics/07_datesinJS.js
+++ 01_Basics/07_datesinJS.js
@@ -1,1 +1,33 @@
+// Date
+let myDate = new Date() ;
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof(myDate));
+
+// let mycreatedDate = new Date(2024, 0,23,5,7);
+// let mycreatedDate = new Date("2024-01-23");
+let mycreatedDate = new Date("01-23-2024");
+// console.log(mycreatedDate.toDateString());
+console.log(mycreatedDate.toLocaleString());
+
+
+let myTimestamp = Date.now()
+
+
+// console.log(myTimestamp);
+// console.log(mycreatedDate.getTime());
+// console.log(Date.now()/1000);
+
+let newDate = new Date("01-23-2024")
+// console.log(newDate.getDate());
+// console.log(newDate.getMonth() +1);
+// console.log(newDate.getFullYear());
+
+ let a = newDate.toLocaleString('default', {
+  weekday:"long",
+})
+
+console.log(a);
+
 
