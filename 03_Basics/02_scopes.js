f2086c844d81b7bee334ebdf633d254e9b39a9c3
Sainath More <166020011+saiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-06-21 01:18:00 +0530

    Update 02_scopes.js

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -23,3 +23,43 @@ console.log(anotherBlockScopedVar); // ReferenceError: anotherBlockScopedVar is 
 //  Means You can globally  access the var from any of the any part of the programm
 //  But let and const are block scoped means you can only access them within the block they are declared in 
 
+
+// Nested Scope {}
+function one(){
+  const username = "Sainath";
+
+  function two(){
+    const website = "youtube";
+    // console.log(username);
+    
+  }
+  // console.log(website);
+  two()
+  
+}
+one()
+
+
+if(true){
+  let username = "Sainath";
+  if(username === "Sainath"){
+      let website = " youtube"
+      // console.log(username + website);
+      
+      // console.log(`This is Website${website}`);
+    }
+  
+    // console.log(`This is Username ${username}`);
+}
+
+// ****************************************************************************************************************
+console.log(addone(1));
+function addone(num){
+  return num +1;
+}
+
+console.log(addtwo(1));
+const func = function addtwo(num){ // when ever you declare a function with the help of variable or identifier you cannot call that function before declratartion because its an exxpression
+  return num + 2;
+}
+
