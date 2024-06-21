94702935ca9e02654700593df7d36c90bff5e8b3
Sainath More <166020011+saiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-06-22 01:25:52 +0530

    Add files via upload

---
+++ 05_iterators_loops/two.js
@@ -0,0 +1,28 @@
+// While loop
+
+// let i = 0;
+// while (i <= 20) {
+//   console.log(`value of i is ${i}`);
+//    i++;
+// }
+
+// let myarray = ["IronMan","superMan","Batman"];
+// let i = 0;
+// while (i < myarray.length) {
+//   console.log(`The First element of myarray is  ${myarray[i]}`)
+//   i++;
+// }
+
+
+// let score = 0;
+// do {
+//   console.log(`Score is ${score}`)
+//   score++;
+// } while (score <= 10);
+
+// let myarray = ["Ironman","BatMan","Superman"]
+// let i = 0;
+// do {
+//   console.log(`The Superhero of my array is ${myarray[i]}`)
+//   i++
+// } while (i < myarray.length);
